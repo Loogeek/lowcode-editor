db4f003c9e5a144618b18607c73a5a3e696a6f67
Loogeek <xiaolong3956@gmail.com>
2024-08-26 11:05:36 +0800

    feat: add event

--- src/components/Setting/components/Event/index.tsx
+++ src/components/Setting/components/Event/index.tsx
@@ -1,5 +1,50 @@
-function ComponentEvent() {
-  return <div>ComponentEvent</div>;
-}
+import { Collapse, Input, Select, CollapseProps } from "antd";
+import { useComponentsStore } from "@/stores/components";
+import { useComponentConfig } from "@/stores/component-config";
 
-export default ComponentEvent;
+export function ComponentEvent() {
+  const { curComponent, curComponentId, updateComponentProps } =
+    useComponentsStore();
+  const { componentConfig } = useComponentConfig();
+
+  if (!curComponent) return null;
+
+  const selectAction = (eventName: string, value: string) => {
+    if (!curComponentId) return;
+
+    updateComponentProps(curComponentId, { [eventName]: { type: value } });
+  };
+
+  const items: CollapseProps["items"] = (
+    componentConfig[curComponent.name].events || []
+  ).map((event) => {
+    return {
+      key: event.name,
+      label: event.label,
+      children: (
+        <div>
+          <div className="flex items-center">
+            <div>动作：</div>
+            <Select
+              className="w-[160px]"
+              options={[
+                { label: "显示提示", value: "showMessage" },
+                { label: "跳转链接", value: "goToLink" },
+              ]}
+              onChange={(value) => {
+                selectAction(event.name, value);
+              }}
+              value={curComponent?.props?.[event.name]?.type}
+            />
+          </div>
+        </div>
+      ),
+    };
+  });
+
+  return (
+    <div className="px-[10px]">
+      <Collapse className="mb-[10px]" items={items} />
+    </div>
+  );
+}
